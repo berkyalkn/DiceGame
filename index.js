5bbe5d62cc9edc31ab0546e23733a86a85d4714b
Berkay Alkan <berky.alknn@gmail.com>
2024-08-28 20:04:12 +0300

    add player score

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-let randomNumber1 = Math.ceil(Math.random() * 7);
-let randomNumber2 = Math.ceil(Math.random() * 7);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var images1 = "images/dice" + randomNumber1 + ".png";
-var images2 = "images/dice" + randomNumber2 + ".png";
+let images1 = "images/dice" + randomNumber1 + ".png";
+let images2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".dice img").setAttribute("src", "images1");
-document.querySelector(".dice img").setAttribute("src", "images2");
+document.querySelectorAll("img")[0].setAttribute("src", images1);
+document.querySelectorAll("img")[1].setAttribute("src", images2);
 
 if(randomNumber1 === randomNumber2) {
     document.querySelector("h2").innerHTML = "🚩 Draw 🚩";
@@ -19,3 +19,6 @@ else if(randomNumber1 > randomNumber2) {
 else {
     document.querySelector("h2").innerHTML = "Player 2 Wins! 🚩";
 }
+
+document.querySelectorAll(".dice > p")[0].innerHTML = "1st Player Score = " + randomNumber1;
+document.querySelectorAll(".dice > p")[1].innerHTML = "2nd Player Score = " + randomNumber2;
